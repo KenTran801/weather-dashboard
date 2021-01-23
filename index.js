5734d48a7d5ea376a178f5a8ad5bd9fed7a17ea6
KenTran801 <utes.fan.tran@gmail.com>
2021-01-23 15:07:37 -0700

    Added code to js file to pull from API.

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ $("#submitBtn").click(function (event) {
     event.preventDefault();
 
     var cityName = $("#citySearch").val();
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=346f5b9d83ff18900a4fdcdbc47dcbde";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=346f5b9d83ff18900a4fdcdbc47dcbde";
 
     $.ajax({
         url: queryURL,
         method: "GET",
-      }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
 
+
+        
     });
 
 });
