82a84bad7e2024c745db9509641d80b02b6fa944
KenTran801 <utes.fan.tran@gmail.com>
2021-01-23 14:17:27 -0700

    Begin working on js file.

---
+++ index.js
@@ -0,0 +1,15 @@
+$("#submitBtn").click(function (event) {
+    event.preventDefault();
+
+    var cityName = $("#citySearch").val();
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=346f5b9d83ff18900a4fdcdbc47dcbde";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function(response) {
+        console.log(response);
+
+    });
+
+});
